07a970c4ed66ea4cd703591c135884baecdc1957
langyo <m13776491897@163.com>
2019-02-04 00:11:39 +0800

    :shit:

---
+++ src/P1046.cpp
@@ -0,0 +1,37 @@
+/*
+        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+
+ Everyone is permitted to copy and distribute verbatim or modified 
+ copies of this license document, and changing it is allowed as long 
+ as the name is changed. 
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+*/
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main(){
+    array<int, 10> arr; // 苹果高度数组
+    for(int i = 0; i < 10; ++i) cin >> arr[i];
+    int h; // 陶陶的身高
+    cin >> h;
+
+    // 预处理，统统减 30 cm
+    for(int i = 0; i < 10; ++i) arr[i] -= 30;
+
+    // 开始统计
+    int count = 0;
+    for(int i = 0; i < 10; ++i) if(arr[i] <= h) count += 1;
+
+    cout << count;
+    return 0;
+}
---
+++ src/P1423.cpp
@@ -0,0 +1,29 @@
+/*
+        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+
+ Everyone is permitted to copy and distribute verbatim or modified 
+ copies of this license document, and changing it is allowed as long 
+ as the name is changed. 
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double in, length = 2.0, ans = 0.0; // length 为当前步能游的距离，ans 为已游总距离
+    int step = 0; // step 为计量的步数
+    cin >> in;
+
+    for(; ans < in; ans += length, length *= 0.98, ++step);
+    cout << step;
+    return 0;
+}
