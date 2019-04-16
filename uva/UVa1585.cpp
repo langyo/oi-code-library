2c65638df7432d5254e465ea4e6050b44919d2d1
langyo <langyo.china@gmail.com>
2019-04-16 17:55:34 +0800

    :shit:

---
+++ uva/UVa1585.cpp
@@ -0,0 +1,42 @@
+/*
+        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+
+ Everyone is permitted to copy and distribute verbatim or modified 
+ copies of this license document, and changing it is allowed as long 
+ as the name is changed. 
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int task_count;
+    cin >> task_count;
+
+    for(int t = 0; t < task_count; ++t){
+        string n;
+        int sum = 0, count = 0;
+
+        cin >> n;
+        for(auto i : n){
+            if(i == 'O'){
+                ++count;
+                sum += count;
+            }else{
+                count = 0;
+            }
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
