5ebb0387ae6a29fd19f770229b97254042ee3af4
langyo <langyo.china@gmail.com>
2019-07-18 02:13:19 +0800

    :shit

--- luogu/P1036.cpp
+++ luogu/P1036.cpp
@@ -21,16 +21,20 @@
 #include <functional>
 #include <numeric>
 
+#define DEBUG
+#include "../big_baby.h"
+
 using namespace std;
 
 int main() {
-    int count;
-    cin >> count;
+    int n, k;
+    cin >> n >> k;
 
-    vector<int> list(count, 0);
+    vector<int> list(n, 0);
     for(auto &i : list) cin >> i;
 
     int filter_max_prime_num = accumulate(list.begin(), list.end(), 0);
+    _(filter_max_prime_num);
 
     // 预缓存素数列表
     vector<bool> prime_num_list(filter_max_prime_num + 1, true);
@@ -40,9 +44,26 @@ int main() {
         }
     }
 
-    auto dfs = [&]() {
+    typedef decltype(list.begin()) I;
+    int count = 0;
 
+    function<void(I, I, int, int)>  dfs = [&](I begin, I end, int sum, int last_k) {
+	_print("Enter a new stack level:");
+	_(sum), _(last_k);
+	_print("------");
+	if(last_k == 0) {
+	    _print("DFS succeed.");
+	    if(prime_num_list[sum]) _print("Get a new prime number"), _(sum), count += 1, _(count);
+	    return;
+	}
+	for(auto i = begin; i != end; ++i) {
+	    dfs(i + 1, end, *i + sum, last_k - 1);
+	}
+	_print(begin == end ? "DFS failed." : "");
     };
+    dfs(list.begin(), list.end(), 0, k);
+
+    cout << count << endl;
 
     return 0;
 }
