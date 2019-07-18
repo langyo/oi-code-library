637bd590391f8c21f8c8a547848c90f401ced25a
langyo <langyo.china@gmail.com>
2019-07-18 18:51:07 +0800

    :shit: AC!

--- luogu/P1149.cpp
+++ luogu/P1149.cpp
@@ -10,15 +10,9 @@
   0. You just DO WHAT THE FUCK YOU WANT TO.
 */
 
-// 该版本只能部分 AC
-
-#include <cassert>
 #include <iostream>
 #include <array>
 #include <vector>
-#include <utility>
-#include <sstream>
-#include <stdexcept>
 
 #define DEBUG
 #include "../big_baby.h"
@@ -28,43 +22,41 @@ using namespace std;
 int main(){
 	constexpr array<int, 10> numCount = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
 	
-	int n;
-	cin >> n;
+	int k;
+	cin >> k;
 	
-	if(n <= 10){
+	if(k <= 10){
 		// 能够生成最基本式子的火柴棍个数必须最少也得 10 根（加号、等号、三个一） 
 		cout << 0 << flush;
 		return 0;
 	}
-	n -= 4; // 去掉加号与乘号
+	k -= 4; // 去掉加号与等号
+
+	// 累计满足条件的等式数量
+	int count = 0;
 	
-	// 生成所有有效的候选等式（这部分其实可以打表）
+	// 生成所有有效的候选等式
 	typedef pair<pair<int, int>, int> expr_type;
 	vector<expr_type> match_list;
-	for(int left = 0; left < 10; ++left) {
-		for(int right = 0; right < 10; ++right) {
-			if(left + right < 10) {
-				match_list.push_back(make_pair(make_pair(left, right), left + right));
+	for(int left = 0; left < 1000; ++left) {
+		for(int right = 0; right < 1000; ++right) {
+			// 取得各个位的火柴棒个数并加起来
+			int n = 0;
+			if(left == 0) n += numCount[0];
+			else for(int i = left; i != 0; n += numCount[i % 10], i /= 10);
+			if(right == 0) n += numCount[0];
+			else for(int i = right; i != 0; n += numCount[i % 10], i /= 10);
+			if(left + right == 0) n += numCount[0];
+			else for(int i = left + right; i != 0; n += numCount[i % 10], i /= 10);
+			// 判定是否与所给根数一样
+			if(n == k) {
+				#ifdef DEBUG
+				cout << left << " + " << right << " = " << left + right << " {" << n << "}" << endl;
+				#endif
+				count += 1;
 			}
 		}
 	}
-
-	// 计算候选等式需要的火柴棍个数（不包含加号与等号的 4 根）
-	typedef pair<expr_type, int> state_type;
-	vector<state_type> state_list;
-	for(auto &i : match_list) {
-		int sum = numCount[i.first.first] + numCount[i.first.second] + numCount[i.second];
-		state_list.push_back(make_pair(i, sum));
-	}
-	#ifdef DEBUG
-	for(auto &n : state_list) {
-		cout << n.first.first.first << " + " << n.first.first.second << " = " << n.first.second << " {" << n.second << " + 4 = " << n.second + 4 <<"}" << endl;
-	}
-	#endif
-
-	// 过滤，统计
-	int count = 0;
-	for(auto &i : state_list) if(i.second == n) count += 1;
 
 	// 输出结果
 	cout << count << endl;
