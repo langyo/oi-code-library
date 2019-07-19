a1a15e7519591cf3a2bafd326c0abde490c4f5e6
langyo <langyo.china@gmail.com>
2019-07-19 17:05:18 +0800

    :shit:

---
+++ luogu/P1464.cpp
@@ -0,0 +1,87 @@
+/*
+        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+
+ Everyone is permitted to copy and distribute verbatim or modified 
+ copies of this license document, and changing it is allowed as long 
+ as the name is changed. 
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+*/
+
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <utility>
+
+#define DEBUG
+#include "../big_baby.h"
+
+using namespace std;
+
+struct args_t {
+    long long a, b, c;
+    args_t(long long _a, long long _b, long long _c) : a(_a), b(_b), c(_c) {}
+
+    bool check(int x, int y, int z) {
+        return x == a && y == b && z == c;
+    }
+};
+
+int main() {
+    vector<args_t> init_list;
+    long long inA = 0, inB = 0, inC = 0;
+    cin >> inA >> inB >> inC;
+    while (!(inA == -1 && inB == -1 && inC == -1)) {
+        args_t args(inA, inB, inC);
+        init_list.push_back(args);
+        cin >> inA >> inB >> inC;
+    }
+
+    // 纯函数执行缓存
+    vector<pair<args_t, long long>> cache;
+
+    // 纯函数 w
+    function<long long(long long, long long, long long)> w = [&](long long a, long long b, long long c) -> long long {
+        // 检查缓存
+        for (auto &i : cache) {
+            if (i.first.check(a, b, c)) {
+                _print("Using cache:");
+                _(a), _(b), _(c);
+                _(i.second);
+                return i.second;
+            }
+        }
+
+        // 缓存中没有，现场计算
+        if (a <= 0 || b <= 0 || c <= 0)
+            return 1;
+        if (a > 20 || b > 20 || c > 20)
+            return w(20, 20, 20);
+
+        int ret;
+        if (a < b && b < c) {
+            ret = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+            args_t args(a, b, c);
+            cache.push_back(make_pair(args, ret));
+            return ret;
+        }
+        ret = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        args_t args(a, b, c);
+        cache.push_back(make_pair(args, ret));
+        return ret;
+    };
+
+    // 正式开始计算
+    for (auto &i : init_list) {
+        int ret = w(i.a, i.b, i.c);
+        cout << "w(" << i.a << ", " << i.b << ", " << i.c << ") = " << ret << endl;
+    }
+
+    return 0;
+}
---
+++ luogu/P1579.cpp
@@ -0,0 +1,64 @@
+/*
+        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+                    Version 2, December 2004 
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+
+ Everyone is permitted to copy and distribute verbatim or modified 
+ copies of this license document, and changing it is allowed as long 
+ as the name is changed. 
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+*/
+
+#include <iostream>
+#include <vector>
+
+#define DEBUG
+#include "../big_baby.h"
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // 预缓存素数列表
+    vector<bool> prime_num_list(n + 1, true);
+    for(int i = 2; i <= n; ++i) {
+        if(prime_num_list[i]) {
+            for(int j = i * 2; j < n; j += i) prime_num_list[j] = false;
+        }
+    }
+    prime_num_list[0] = false;
+    prime_num_list[1] = false;
+
+    // 哥德巴赫猜想所指示的 3 个素数有两种情况（给出的数字 > 9）：
+    // [1] 两个偶数，一个奇数
+    // [2] 三个奇数
+
+    // 情况 [1]
+    // 因为只有 2 是偶数中的素数，所以前两个数字必定为 2，只需要再判断最后的 n - 4 是否为素数即可
+    if(prime_num_list[n - 4]) {
+        cout << 2 << " " << 2 << " " << n - 4 << endl;
+        return 0;     // 直接结束程序
+    }
+
+    // 情况 [2]
+    // 前两个数字暴力枚举，不过限定范围在奇数集，第三个数字直接减，然后逐个检查
+    for(int a = 3; a <= n; a += 2) {
+        for(int b = 3; b <= n; b += 2) {
+            int c = n - a - b;
+            if(prime_num_list[a] && prime_num_list[b] && prime_num_list[c]) {
+                cout << a << " " << b << " " << c << endl;
+                return 0;   // 直接结束程序
+            }
+        }
+    }
+
+    _print("Wait...Why did the program go there?!");
+    return 0;
+}
