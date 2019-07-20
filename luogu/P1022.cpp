0f9b497ea5c4cfcf313464bd3af09522cc33730b
langyo <langyo.china@gmail.com>
2019-07-20 19:11:46 +0800

    :shit: 新手村全部通过！

--- luogu/P1022.cpp
+++ luogu/P1022.cpp
@@ -17,7 +17,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <sstream>
 #include <iomanip>
 
 #define DEBUG
@@ -31,103 +31,82 @@ int main() {
     string expr;
     getline(cin, expr);
 
-    vector<int> left_unknown_weight, right_unknown_weight;
-    vector<int> left_constant, right_constant;
-    char alpha;
+    bool isPlus = true;     // 当前正负情况；如果为真则符号为加，否则为减
+    bool isLeft = true;     // 当前等式方向；如果为真则为左半部分，否则为右半部分
+    bool isUnknown = false; // 当前数字归属状态；如果为真则为未知数系数，否则为常数项
+    bool hasNum = false;    // 当前状态是否已填写过数字；这是判断未知数是否有系数的依据，用于自动填充未知数系数为 1
+    long long num = 0;      // 数字缓存
 
-    string left = expr.substr(0, expr.find('='));
-    string right = expr.substr(expr.find('=') + 1, expr.size());
-    _(left), _(right);
+    // 注意，下面的两个累积和都是算作等式左边的，这是为了减少判断部分的复杂度
+    int unknown_weight = 0; // 未知数权值
+    int constant = 0;       // 常数值
+    char alpha = 'a';        // 未知数符号
 
-    auto parse = [&](const string &expr, vector<int> &unknown_weight, vector<int> &constant) {
-        // 判断开头，此时开头只可能有 - 或任何符号都没有
-        string::size_type current_begin_pos;
-        _print("New progress");
-        if(expr[0] != '-') {
-            // 开头为正数
-            current_begin_pos = min(expr.find('+'), expr.find('-'));
-            string part = expr.substr(0, min(expr.find('+'), expr.find('-')));
-            if('a' <= part.back() && part.back() <= 'z') {
-                alpha = part.back();
-                unknown_weight.push_back(stoi(part.substr(0, part.size() - 1)));
-                _for_print(unknown_weight, [](int n) -> int { return n; });
-            }
-            else {
-                constant.push_back(stoi(part));
-                _for_print(constant, [](int n) -> int { return n; });
-            }
-        } else {
-            // 开头为负数
-            current_begin_pos = min(expr.find('+', 1), expr.find('-', 1));
-            string part = expr.substr(1, min(expr.find('+', 1), expr.find('-', 1)));
-            if('a' <= part.back() && part.back() <= 'z') {
-                alpha = part.back();
-                unknown_weight.push_back(-stoi(part.substr(0, part.size() - 1)));
-                _for_print(unknown_weight, [](int n) -> int { return n; });
-            }
-            else {
-                constant.push_back(-stoi(part));
-                _for_print(constant, [](int n) -> int { return n; });
-            }
-        }
-        // 判断中间部分，此时开头必定会有 + 或 -
-        _(expr);
-        _(current_begin_pos);
-        for(
-         string::size_type i = min(expr.find('+', current_begin_pos + 1), expr.find('-', current_begin_pos + 1)),
-         last_i = current_begin_pos;;
-         last_i = i, i = min(expr.find('+', i + 1), expr.find('-', i + 1))
-        ) {
-            string part;
-            _(i == string::npos ? 't' : 'f');
-            if(i != string::npos) part = expr.substr(last_i, i);
-            else {
-                _(expr);
-                _(expr.size());
-                part = expr.substr(last_i, expr.size());
-            }
-            _(last_i), _(i);
-            _(part);
-            if(part.front() == '+') {
-                if('a' <= part.back() && part.back() <= 'z') {
-                    alpha = part.back();
-                    unknown_weight.push_back(stoi(part.substr(1, part.size() - 1)));
-                    _for_print(unknown_weight, [](int n) -> int { return n; });
-                } else {
-                    constant.push_back(stoi(part.substr(1, part.size())));
-                    _for_print(constant, [](int n) -> int { return n; });
-                }
-            } else if(part.front() == '-') {
-                if('a' <= part.back() && part.back() <= 'z') {
-                    alpha = part.back();
-                    unknown_weight.push_back(-stoi(part.substr(1, part.size() - 1)));
-                    _for_print(unknown_weight, [](int n) -> int { return n; });
-                } else {
-                    constant.push_back(-stoi(part.substr(1, part.size())));
-                    _for_print(constant, [](int n) -> int { return n; });
-                }
-            } else if(i == string::npos) break;
-            else _print("Error!");
+    // 运算器函数
+    auto evaluator = [&]() {
+        if((isPlus && isLeft) || ((!isPlus) && (!isLeft))) {
+            // 在左侧且为加法，或在右侧且为减法
+            if(isUnknown && hasNum) unknown_weight += num;
+            else if(isUnknown && (!hasNum)) __, unknown_weight += 1;
+            else constant += num;
+        } else if(((!isPlus) && isLeft) || (isPlus && (!isLeft))) {
+            // 在左侧且为减法，或在右侧且为加法
+            if(isUnknown && hasNum) unknown_weight -= num;
+            else if(isUnknown && (!hasNum)) __, unknown_weight -= 1;
+            else constant -= num;
         }
     };
 
-    parse(left, left_unknown_weight, left_constant);
-    parse(right, right_unknown_weight, right_constant);
-    
-    _print("Reducing...");
-    _for_print(left_unknown_weight, [](int n) -> int { return n; });
-    _for_print(left_constant, [](int n) -> int { return n; });
-    _for_print(right_unknown_weight, [](int n) -> int { return n; });
-    _for_print(right_constant, [](int n) -> int { return n; });
+    // 遍历整个字符串
+    for(string::size_type i = 0; i < expr.size(); ++i) {
+        char ch = expr[i];  // 当前游标所在字符
+        _(ch), _(isLeft), _(isPlus), _(isUnknown), _(hasNum), _(num), _(unknown_weight), _(constant);
+        if(ch == '+') {
+            // 处理上一个符号对应数字的加减操作
+            evaluator();
+            // 设置下一状态
+            isPlus = true; 
+            isUnknown = false;
+            hasNum = false;
+            num = 0;
+        }
+        if(ch == '-') {
+            // 处理上一个符号对应数字的加减操作
+            evaluator();
+            // 设置下一状态
+            isPlus = false; 
+            isUnknown = false;
+            hasNum = false;
+            num = 0;
+        }
+        if(ch == '=') {
+            // 处理上一个符号对应数字的加减操作
+            evaluator();
+            // 设置下一状态
+            isLeft = false;
+            isUnknown = false;
+            isPlus = true;
+            hasNum = false;
+            num = 0;
+        }
+        if('a' <= ch && ch <= 'z') {
+            alpha = ch;
+            isUnknown = true;
+        }
+        if('0' <= ch && ch <= '9') {
+            hasNum = true;
+            num = num * 10 + static_cast<int>(ch) - static_cast<int>('0');
+        }
+    }
+    // 最后一次处理数字加减操作
+    evaluator();
 
-    double unknown_weight_sum = 0, constant_sum = 0;
-    for(auto &i : left_unknown_weight) unknown_weight_sum += i;
-    for(auto &i : right_unknown_weight) unknown_weight_sum -= i;
-    for(auto &i : left_constant) constant_sum -= i;
-    for(auto &i : right_constant) constant_sum += i;
-    _(unknown_weight_sum), _(constant_sum);
+    // 计算结果
+    double result;
+    _(unknown_weight), _(constant);
+    if(unknown_weight == 0 || constant == 0) result = 0.0;
+    else result = - static_cast<double>(constant) / static_cast<double>(unknown_weight);
 
-    double result = constant_sum / unknown_weight_sum;
-    cout << setprecision(3) << result << endl;
+    cout << alpha << "=" << setiosflags(ios::fixed) << setprecision(3) << result << endl;
     return 0;
 }
