222bf72242fbe700ad7f5999b7e52379a01d31bd
langyo <langyo.china@gmail.com>
2019-07-23 00:19:15 +0800

    :shit:

--- luogu/P1219.cpp
+++ luogu/P1219.cpp
@@ -29,6 +29,7 @@ int main() {
 
     vector<int> pos;    // pos 下标为行编号，pos 下标下的值为列编号
     vector<vector<int>> result;
+    int count = 0;
 
     auto check = [&]() -> bool {
         // 只要逐个检查各个排列是否能保证每个子对角线内没有其它子即可
@@ -38,7 +39,7 @@ int main() {
         vector<int> leftDownToRightUp;
         // 计算每个当前棋子的标记值，并检查两组向量中是否有重复数字，如果有则放弃该方案
         bool continueFlag = false;
-        for(int i = 0; i < n; ++i) {
+        for(int i = 0; i < pos.size(); ++i) {
             int row = i + 1, column = pos.at(i);
             // 检查左上 - 右下
             for(auto i : leftUpToRightDown) {
@@ -65,8 +66,12 @@ int main() {
 
     function<void(int)> dfs = [&](int t) {
         if(t == n) {
-            if(check()) result.push_back(pos);
+            if(check()) {
+		count += 1;
+		if(count <= 3) result.push_back(pos);
+	    }
         } else {
+	    if(!check()) return;
             for(int i = 1; i <= n; ++i) {
                 // 检查是否重复，如有重复，直接跳过这一情况
                 bool isRepeat = false;
@@ -87,9 +92,9 @@ int main() {
 
     dfs(0);
 
-    for(int i = 0; i < 3; ++i) {
-        for(auto j = result.at(i).cbegin(); j != result.at(i).cend() - 1; ++j) cout << *j << " ";
-        cout << result.at(i)[result.at(i).size() - 1] << endl; 
+    for(auto &i : result) {
+        for(auto j = i.cbegin(); j != i.cend() - 1; ++j) cout << *j << " ";
+        cout << i[i.size() - 1] << endl; 
     }
     cout << result.size() << endl;
 
